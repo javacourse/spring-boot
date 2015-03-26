bc6d2d4634cdd5f778358ca2f9f36b5a13e4715c
Max Bagrov <wisdom683@rambler.ru>
2015-03-26 15:38:02 +0300

    Authentication using database tables

---
+++ src/main/java/com/mbagrov/service/api/PersonService.java
@@ -0,0 +1,7 @@
+package com.mbagrov.service.api;
+
+/**
+ * Created by Odour on 26.03.2015.
+ */
+public interface PersonService {
+}
---
+++ src/main/java/com/mbagrov/service/impl/PersonServiceImpl.java
@@ -0,0 +1,9 @@
+package com.mbagrov.service.impl;
+
+import com.mbagrov.service.api.PersonService;
+
+/**
+ * Created by Odour on 26.03.2015.
+ */
+public class PersonServiceImpl implements PersonService {
+}
